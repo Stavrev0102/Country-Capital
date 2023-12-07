985a36755458d8736546db9dd14baa8856238c06
Ivaylo Stavrev <114228905+Stavrev0102@users.noreply.github.com>
2023-12-07 18:46:21 +0200

    ...
    If there are no more countries or capitals left, I win. When I click on the correct combination, both the capital and country are filtered from all records. If it's the wrong combination, their backgrounds turn red, and you should then click on the correct button.

--- country-capital/src/components/country-capital/country-capital.jsx
+++ country-capital/src/components/country-capital/country-capital.jsx
@@ -14,26 +14,57 @@ export default function CountryCapital({data}){
         }))
         .sort(() => Math.random() - 0.5)
     )
+
+    const [selected,setSelected] = useState(undefined);
+    const isGameOver = options.length === 0;
+
+    if(isGameOver) {
+       return <h1>Congratulations!</h1>
+    }
     
-    // options.sort(() => Math.random() - 0.5)
-    console.log(options);
     return (
         <div>
             {options.map((option) => (
                 // eslint-disable-next-line react/jsx-key
                 <button 
-                className={option.state === 'SELECTED' ? "selected" : "" }
+                className={option.state === 'SELECTED' ? "selected" : option.state === 'WRONG' ? 'wrong' : '' }
+                key={option.value}
                 onClick={() => {
-                    setOptions(
-                        options.map((opt) => {
-                        return opt === option 
-                        ? {
-                            ...opt,
-                            state:'SELECTED'
-                        } 
-                        : opt;
-                    })
-                    )
+                    if(!selected){
+                        setSelected(option)
+                        setOptions(
+                            options.map((opt) => {
+                            return opt === option 
+                            ? {
+                                ...opt,
+                                state:'SELECTED'
+                            } 
+                            : {...opt,state:'DEFAULT'};
+                        })
+                        )
+                    } else {
+                        if(selected.value === data[option.value] || data[selected.value] === option.value) {
+                            setOptions(
+                                options.filter((opt) => {
+                                    return !(
+                                        opt.value === selected.value || opt.value === option.value
+                                    )
+                                })
+                            )
+                        } else {
+                            //wrong combination
+                            setOptions(
+                                options.map((opt) => {
+                                    return (
+                                        opt.value === selected.value || opt.value === option.value
+                                    ) ? {...opt,state:'WRONG'} 
+                                    :opt
+                                })
+                            )
+                        }
+                        setSelected(undefined)
+                    }
+                    
                 }}
                 >{option.value}</button>
             ) )}
